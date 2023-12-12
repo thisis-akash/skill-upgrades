ffb5d7938ad1a1ac83a163641f9b20339f510a14
thisis-akash <30195464+thisis-akash@users.noreply.github.com>
2023-12-12 23:21:07 +0530

    added infinite scroll to user story feed

--- App2_Social_Media_Instagram/App.js
+++ App2_Social_Media_Instagram/App.js
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   SafeAreaView, View, TouchableOpacity, FlatList
 } from 'react-native';
@@ -16,8 +16,14 @@ import Title from './components/Title/Title';
 import globalStyle from './assets/styles/globalStyle';
 import UserStory from './components/UserStory/UserStory';
 
+const APP_TITLE = `Let's Explore`;
+const initialState = {
+  storiesFeedCurrentPage: 1,
+  storiesFeedRenderedData: [],
+  isLoadingStoriesFeed: false
+};
+
 function App() {
-  const APP_TITLE = `Let's Explore`;
   const userStories = [{
     id: 1,
     firstName: 'User 1',
@@ -56,26 +62,82 @@ function App() {
     profileImage: require('./assets/images/default_profile.png')
   }];
 
+  const storiesFeedPageSize = 4;
+  const [storiesFeedCurrentPage, setStoriesFeedCurrentPage] = useState(initialState.storiesFeedCurrentPage);
+  const [storiesFeedRenderedData, setStoriesFeedRenderedData] = useState(initialState.storiesFeedRenderedData);
+  const [isLoadingStoriesFeed, setIsLoadingStoriesFeed] = useState(initialState.isLoadingStoriesFeed);
+
+  useEffect(() => {
+
+    setIsLoadingStoriesFeed(true);
+
+    const getInitialData = pagination(userStories, 1, storiesFeedPageSize);
+    setStoriesFeedRenderedData(getInitialData);
+
+    setIsLoadingStoriesFeed(false);
+
+  }, []);
+
+  const pagination = (data, currentPage, pageSize) => {
+    const startIndex = (currentPage - 1) * pageSize;
+    const endIndex = startIndex + pageSize;
+
+    return (startIndex >= data.length) ? [] : data.slice(startIndex, endIndex);
+  };
+
+  const loadMoreStories = () => {
+    if (isLoadingStoriesFeed) return;
+
+    setIsLoadingStoriesFeed(true);
+
+    const contentToAppend = pagination(
+      userStories,
+      storiesFeedCurrentPage + 1,
+      storiesFeedPageSize,
+    );
+
+    if (contentToAppend.length) {
+      setStoriesFeedCurrentPage(storiesFeedPageSize + 1);
+      setStoriesFeedRenderedData(prev => [...prev, ...contentToAppend]);
+    }
+
+    setIsLoadingStoriesFeed(false);
+  }
+
   return (
     <SafeAreaView>
 
 
       <View style={globalStyle.header}>
 
+
         <Title title={APP_TITLE} />
+
         <TouchableOpacity style={globalStyle.messageIcon}>
           <FontAwesomeIcon icon={faEnvelope} color='#898DAE' size={20} />
         </TouchableOpacity>
+
 
       </View>
 
 
       <View style={globalStyle.userStoryContainer}>
 
-        <FlatList showsHorizontalScrollIndicator={false} horizontal={true}
-          data={userStories} renderItem={({ item }) => (
-            <UserStory firstName={item.firstName} profileImage={item.profileImage} />)} >
+
+        <FlatList
+          showsHorizontalScrollIndicator={false}
+          horizontal={true}
+          onEndReachedThreshold={0.5}
+          onEndReached={() => loadMoreStories()}
+          data={storiesFeedRenderedData}
+
+          renderItem={({ item }) => {
+            const storyKey = `story_${item.id}`;
+            return <UserStory key={storyKey} firstName={item.firstName} profileImage={item.profileImage} />
+          }} >
+
         </FlatList>
+
 
       </View>
 
