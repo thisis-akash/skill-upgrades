27f9c44bb8e29a6638e8c5eadfe29864610c9f35
thisis-akash <30195464+thisis-akash@users.noreply.github.com>
2023-12-16 22:00:55 +0530

    added main navigation/drawer navigation to the app

--- App2_Social_Media_Instagram/navigation/MainNavigation.js
+++ App2_Social_Media_Instagram/navigation/MainNavigation.js
@@ -1,20 +1,32 @@
 import { createStackNavigator } from "@react-navigation/stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
 
 import { Routes } from "./Routes";
 import Home from '../screens/Home/Home';
 import Profile from "../screens/Profile/Profile";
 
 const Stack = createStackNavigator();
+const Drawer = createDrawerNavigator();
+
+const MainMenuNavigation = () => {
+
+    return (
+        <Drawer.Navigator screenOptions={{ headerShown: false }} >
+            <Drawer.Screen name={Routes.Home} component={Home} />
+            <Drawer.Screen name={Routes.Profile} component={Profile} />
+        </Drawer.Navigator>
+    );
+
+};
 
 const MainNavigation = () => {
 
     return (
         <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={Routes.Home} >
-            <Stack.Screen name={Routes.Home} component={Home} />
-            <Stack.Screen name={Routes.Profile} component={Profile} />
+            <Stack.Screen name={'Drawer'} component={MainMenuNavigation} />
         </Stack.Navigator>
     );
 
-}
+};
 
 export default MainNavigation;
