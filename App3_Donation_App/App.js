36e552bb96f5817b90e6de83c0fd15075dda1389
thisis-akash <30195464+thisis-akash@users.noreply.github.com>
2023-12-17 21:05:54 +0530

    init donation app

---
+++ App3_Donation_App/App.js
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+const App = () => {
+
+  return (
+    <SafeAreaView />
+  );
+
+}
+
+export default App;
